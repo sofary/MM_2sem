23f0284b2e3304d3142247fc76e0d4880998975f
SashaAre <archipova.s@mail.ru>
2019-03-13 00:48:13 +0300

    2_task
    The first try

--- 1.h
+++ 1.h
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
-int** get_matrix(FILE *in, int *n, int *m,int *err);
-void del_matrix(int **a, int n);
-void out_matrix(int **a,int n, int m);
-int task(int **a, int n, int m);
-int del_string(int **a,int n,int max);
-int sravn_stok(int **a, int p, int f,int m);
+#include <stdlib.h>
+#include <string.h>
+
+int search(const char *s, char *cool);
--- main.c
+++ main.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "1.h"
+#include <string.h>
+
+int search(const char *name, char *cool);
 
 int main(void)
 {
-  int n,m, err, **a,l; 
 
-  FILE *in=fopen("data.dat","r");
-  a=get_matrix(in,&n,&m,&err);
-  if(err==-1)
-  {
-    out_matrix(a,n,m); 
-	  printf("\n");
-	  l=task( a,n, m);
-	  if(l==-1 ) 
-	  {out_matrix(a, n,  m);
-	  }
-	  else 	  {
-	    del_string(a, n, l);
-	    n=n-1;
-	    out_matrix(a, n,  m);}
+char *cool;
+cool = malloc( sizeof(char)*512 ); // выделение памяти
+int k;
+const char *s="1.txt";
 
-	    
-	  del_matrix(a,n); }
-  else
-  {
-     if(err==0)
-  {printf("no file");}
-     if(err==1)
-  {printf("wrong sizes of matrix");}    
-     if( err==3)
-  {printf("too many or less elements");}//когда такое возможно??       
-     if(err==2 )
-  {printf("no memory for string");}//когда такое возможно??  
-     if(err==4 )
-  {printf("file is bad");}      
-      // if(err==5 )
-//  {printf("les elements");}   
-  }
-
-  return 0;
+k=search(s, cool);
+if(k==-1) printf("cannot open!");
+else printf("%s",cool);
+free(cool);
 }
