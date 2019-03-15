8ac77c95da47f832be5ced1c3d7edf55236ec5bd
SashaAre <archipova.s@mail.ru>
2019-03-15 07:11:38 +0300

    Done

--- fun2.c
+++ fun2.c
@@ -1,59 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "2.h"
 int search(const char *s, char *cool)
 {
 FILE *in=fopen(s,"r");
-int i=1,k=0,nd,cd;
+int k=0,nd,cd,flag=0;
 char *str1;//считываемая строка
 char *str2;//настоящий элемент
-char *str3;//элемент cool с приписанными 0
+
 char *p;
 str1 = malloc( sizeof(char)*512 );
-str2 = malloc( sizeof(char)*512 );
-str3 = malloc( sizeof(char)*1024 );
+
+memset(cool,0,512);
 
 
 if (in==NULL) return -1;
  while(fgets (str1, 512, in)!=0)
  {	
- 	str2 = strtok(str1," \t");
+
+ 	str2 = strtok(str1," \t\n");
  		while(str2!=NULL)
- 		{//printf("jj  ");
- 			if(k==0) cool=str2;//присваиваем значение первой комбинации цифр в первой строке
 
- 			nd=strlen(str2);//длина текущего слова
- 			cd=strlen(cool);//длина cool
+ 		{   
+		  for(int g=0;g<(int)strlen(str2);g++) 
+ 				if (str2[g]>'9' ||  str2[g]<'0') {flag=1;}//знак не является циврой, +,-
+ 				
+		 if(flag==0)
+ 			{
+			 
+			  printf("\n");
+ 			if(k==0) {strcpy(cool,str2);k=1;}//присваиваем значение первой комбинации цифр в первой строке
 
+ 			nd=(int)strlen(str2);//длина текущего слова
+ 			cd=(int)strlen(cool);//длина cool
+//printf("%s  %s",str2,cool);
  			if(nd>cd)
- 			{p = malloc( sizeof(char)*512 );
- 				for(int k=0;k<nd-cd;k++)
+ 			{ 
+ 				p = malloc( sizeof(char)*512 );
+ 				memset(p,0,512);
+				for(int k=0;k<nd-cd;k++)
  					p[k]='0';
- 				str3=strcat(p,cool);
- 				if(nd==strlen(str3) && strcmp(str3,str2)>0) cool=str2;
+ 				//str3=
+ 				strcat(p,cool);
+				//printf("kek11");
+ 				if(strcmp(p,str2)>0) {strcpy(cool,str2);}
  				free(p);
+
  			}
+ 			else if(nd==cd && strcmp(cool,str2)>0) {strcpy(cool,str2);}
+ 					else if(nd<cd)
+ 					{ 
+ 						p = malloc( sizeof(char)*512 );
+						memset(p,0,512);
+ 					for(int k=0;k<cd-nd;k++)
+ 							p[k]='0';
+					
+					
+ 						strcat(p,str2);
+				
+ 						if( strcmp(p,cool)<0) {strcpy(cool,str2);/*printf("kek33\n");printf("%s",p);*/}
+ 						free(p);
+ 						 
+ 					}
 
-
- 			else if(nd==cd && strcmp(cool,str2)>0) cool=str2;
- 			else if(nd<cd)
- 			{p = malloc( sizeof(char)*512 );
- 			for(int k=0;k<nd-cd;k++)
- 					p[k]='0';
- 				str3=strcat(p,str2);
- 				if(cd==strlen(str3) && strcmp(str3,str2)<0) cool=str2;
- 				free(p);
- 			}
-
- 			k=1;
- 			str2=strtok(NULL, " \t");
- 		} 
+ 					
+ 		}
+ 		 
+ 			str2=strtok(NULL, " \t\n");
+ 			flag=0;
+ 			
+ 	} 
  } 
 
-	printf("%s",cool);
-free(str2);
+	
+//free(str2);
 free(str1);
+//free(str3);
 
 return 0;
 }
--- main2.c
+++ main2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "2.h"
 
-int search(const char *name, char *cool);
+//int search(  const char *name, char *cool);
 
 int main(void)
 {
 
 char *cool;
+//const char *s="1.txt";
 cool = malloc( sizeof(char)*512 ); // выделение памяти
-int k;
-const char *s="1.txt";
 
-k=search(s, cool);
-if(k==-1) printf("cannot open!");
+
+
+
+if(search("1.txt", cool)==-1) printf("cannot open!\n");
 else printf("%s",cool);
 free(cool);
+return 0;
 }
